9d8520ae3e4f6d85062aaddc8495ec9513b5dd49
arun1801 <btrack44@gmail.com>
2018-09-24 14:11:09 +0200

    Update pushImageToRepo.sh

--- pushImageToRepo.sh
+++ pushImageToRepo.sh
@@ -3,6 +3,6 @@ FILETOREAD=tempfile.txt
 while IFS= read -r line
 do
         echo "Pushing $line:latest to Docker Hub..."
-        docker push $line:latest
+        docker push arunsingh1801/$line:latest
         echo "Successfully pushed Docker Image to docker hub..."
 done <"$FILETOREAD"
